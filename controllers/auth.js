0069faebfcf930e64f1b39602c263fbd7d12de16
Mukti DJ <muktidwijatmoko@gmail.comJ>
2022-07-03 12:00:21 +0700

    Add sign in, intsall jwt, login & created JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const { validationResult } = require('express-validator');
 
 const bcrypt = require('bcrypt');
-
+const jwt = require('jsonwebtoken')
 const User = require('../models/user');
 
 exports.signup = (req, res, next) => {
@@ -30,8 +30,48 @@ exports.signup = (req, res, next) => {
     })
     .catch(err => {
         if (!err.statusCode) {
-            err.statusCode = 500;
+            err.statusCode = 501;
           }
           next(err);
     })
+}
+
+
+exports.login = (req, res, next) => {
+    const email = req.body.email;
+    const password  = req.body.password;
+    let loadedUser;
+    User.findOne({ email: email})
+        .then(user => {
+            if(!user) {
+                const error = new Error('A user with this email could not be found');
+                error.statusCode = 401;
+                throw error;
+            }
+            loadedUser = user;
+            return bcrypt.compare(password, user.password)
+        })
+        .then(isEqual => {
+            if(!isEqual) {
+                const error = new Error('Wrong Password');
+                error.statusCode = 401;
+                throw error;
+            }
+            const token = jwt.sign(
+                {
+                email: loadedUser.email,
+                userId: loadedUser._id.toString()
+              },
+              'somesupersecretsecret',
+              { expiresIn: '1h'}
+            );
+            res.status(200).json({ token: token, userId: loadedUser._id.toString()})
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 501;
+              }
+              next(err);
+        })
+
 }
